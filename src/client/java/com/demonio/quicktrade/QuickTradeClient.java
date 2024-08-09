c8173f9448be0e639c04a48602d6016eb02f8efe
Raul Huelamo <raul@huelamo.info>
2024-08-09 20:39:37 +0200

    Initial commit from Fabric Template Mod Generator

---
+++ src/client/java/com/demonio/quicktrade/QuickTradeClient.java
@@ -0,0 +1,10 @@
+package com.demonio.quicktrade;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class QuickTradeClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
